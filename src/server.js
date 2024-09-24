1af92dc1af9d3642b9cef12b8ce1ead3a7e16f01
jonatasdhs <jonatasilveira04@gmail.com>
2024-09-24 15:10:08 -0300

    feat: change awnsers from phrases to numbers

--- src/server.js
+++ src/server.js
@@ -1,15 +1,15 @@
 const { Client, LocalAuth } = require('whatsapp-web.js');
-const qrcode = require('qrcode-terminal')
+const qrcode = require('qrcode-terminal');
 const express = require('express');
 const { readDb, writeDb, updateDb } = require('./utils/dbManipulation');
-const QRCode = require('qrcode')
-const cors = require('cors')
+const QRCode = require('qrcode');
+const cors = require('cors');
 const { join } = require('node:path');
 const { Server } = require("socket.io");
 const { createServer } = require('node:http');
-const { introduction, question, badEnding, confirmationText, confirmation3, confirmation2, goodEnding } = require('./utils/constants');
+const { introduction, question, badEnding, confirmationText, confirmation3, confirmation2, goodEnding, question2 } = require('./utils/constants');
 
-const app = express()
+const app = express();
 const server = createServer(app);
 
 const io = require('socket.io')(server, {
@@ -18,44 +18,57 @@ const io = require('socket.io')(server, {
     }
 });
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-io.on("connection", (socket) => { })
+io.on("connection", (socket) => { });
 
-let qrcodeVar
+let qrcodeVar;
 const client = new Client({
     authStrategy: new LocalAuth()
 });
 
 client.on('ready', () => {
-    io.emit("qr-connected")
+    io.emit("qr-connected");
     console.log('Client is ready!');
 });
 
 function generateQR() {
     return client.on('qr', qr => {
         qrcode.generate(qr, { small: true }, function (qrcode) {
-            qrcodeVar = qrcode
-            console.log(qrcode)
+            qrcodeVar = qrcode;
+            console.log(qrcode);
         });
     });
 }
 
-generateQR()
+generateQR();
 
 client.on('qr', qr => {
     qrcode.generate(qr, { small: true }, function (qrcode) {
-        qrcodeVar = qrcode
+        qrcodeVar = qrcode;
     });
 });
 
-app.post("/sendMessage", async (req, res) => {
-    const contacts = req.body
-    const data = readDb()
-    const user = data.contacts.find((element) => element.phone === contacts.phone)
+// Objeto para armazenar o estado de cada usuário
+let userStates = {};
 
-    if (!user) res.send(404, "User not found!")
+// Função para obter o estado do usuário
+function getUserState(phone) {
+    return userStates[phone] || 'initial'; // Estado inicial se não houver estado salvo
+}
+
+// Função para definir o estado do usuário
+function setUserState(phone, state) {
+    userStates[phone] = state;
+}
+
+app.post("/sendMessage", async (req, res) => {
+    const contacts = req.body;
+    const data = readDb();
+    const user = data.contacts.find((element) => element.phone === contacts.phone);
+
+    if (!user) return res.status(404).send("User not found!");
 
     await client.sendMessage(user.phone + "@c.us", `${introduction(user.name, user.sueNumber, user.company)}`)
         .then((result) => {
@@ -65,76 +78,81 @@ app.post("/sendMessage", async (req, res) => {
             console.error('Error when sending: ', erro); //return object error
         });
 
-    res.send({ user: user, request: req.body })
-})
+    res.send({ user: user, request: req.body });
+});
 
 app.get("/allContacts", async (req, res) => {
-    const contacts = await client.getContacts()
-    res.send(contacts)
-})
+    const contacts = await client.getContacts();
+    res.send(contacts);
+});
 
 app.post("/addContact", async (req, res) => {
-    const newContact = req.body
-    /* const db = readDb() */
-    writeDb(newContact)
-    res.status(201).send(newContact)
-})
+    const newContact = req.body;
+    writeDb(newContact);
+    res.status(201).send(newContact);
+});
 
 app.get("/getContacts", async (req, res) => {
-    const contacts = readDb()
-    res.status(200).send(contacts)
-})
+    const contacts = readDb();
+    res.status(200).send(contacts);
+});
 
 app.get("/qrcode", async (req, res) => {
-    res.status(200).send(qrcodeVar)
-})
+    res.status(200).send(qrcodeVar);
+});
 
 app.get("/healthChecker", async (req, res) => {
-    io.emit("Qr code was read!")
-    res.status(200).send("Hello World!")
-})
+    io.emit("Qr code was read!");
+    res.status(200).send("Hello World!");
+});
 
+// Lógica de controle de estado nas mensagens recebidas
 client.on("message_create", async (message) => {
+    const phone = message.from;
+    const currentState = getUserState(phone);
 
-    if (message.body == "Confirmo") {
-        await client.sendMessage(message.from, confirmationText)
-        client.sendMessage(message.from, question)
-            .then((result) => {
-                console.log('Result: ', result); //return object success
-            })
-            .catch((erro) => {
-                console.error('Error when sending: ', erro); //return object error
-            });
-    }
-
-    if (message.body == "Não") {
-        client.sendMessage(message.from, badEnding)
-        let data = readDb()
-        const findContact = data.contacts.find((contact) => contact.phone === message.from.slice(0, 12))
-        const index = data.contacts.indexOf(findContact)
-        const updatedContact = {
-            ...findContact,
-            status: "recusado"
+    // Estado inicial
+    if (currentState === 'initial') {
+        if (message.body === "1") {
+            await client.sendMessage(phone, confirmationText);
+            await client.sendMessage(phone, question);
+            await client.sendMessage(phone, question2);
+            setUserState(phone, 'waitingForSecondAnswer'); // Muda para o estado da segunda pergunta
+        } else if (message.body === "2") {
+            await client.sendMessage(phone, badEnding);
+            let data = readDb();
+            const findContact = data.contacts.find(contact => contact.phone === phone.slice(0, 12));
+            const index = data.contacts.indexOf(findContact);
+            const updatedContact = {
+                ...findContact,
+                status: "recusado"
+            };
+            data.contacts[index] = updatedContact;
+            updateDb(data);
         }
-        data.contacts[index] = updatedContact
-        updateDb(data)
     }
 
-    if (message.body == "Tenho interesse" || message.body == "Ainda tenho dúvidas") {
-        client.sendMessage(message.from, goodEnding)
-        let data = readDb()
-        const findContact = data.contacts.find((contact) => contact.phone === message.from.slice(0, 12))
-        const index = data.contacts.indexOf(findContact)
-        const updatedContact = {
-            ...findContact,
-            status: "confirmado"
+    // Estado esperando pela segunda resposta
+    else if (currentState === 'waitingForSecondAnswer') {
+        if (message.body === "1" || message.body === "2") {
+            await client.sendMessage(phone, goodEnding);
+            let data = readDb();
+            const findContact = data.contacts.find(contact => contact.phone === phone.slice(0, 12));
+            const index = data.contacts.indexOf(findContact);
+            const updatedContact = {
+                ...findContact,
+                status: "confirmado"
+            };
+            data.contacts[index] = updatedContact;
+            updateDb(data);
+
+            // Depois de finalizar o fluxo, reseta o estado para 'initial'
+            setUserState(phone, 'initial');
         }
-        data.contacts[index] = updatedContact
-        updateDb(data)
     }
-})
+});
 
 server.listen(3000, () => {
     client.initialize();
-    console.log(`Server listening on PORT: 3000`)
-})
+    console.log(`Server listening on PORT: 3000`);
+});
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,11 +1,12 @@
-const introduction = (reclamante, numeroDoProcesso, reclamada) => `Bom dia, ${reclamante}. No processo trabalhista ${numeroDoProcesso} contra a empresa ${reclamada}, você consta como parte. Confirma essa informação? \n Digite: "Confirmo" para prosseguir \n Digite: "Não" para cancelar`
+const introduction = (reclamante, numeroDoProcesso, reclamada) => `Bom dia, ${reclamante}. No processo trabalhista ${numeroDoProcesso} contra a empresa ${reclamada}, você consta como parte. Confirma essa informação? \n\n Envie "1" para confirmar \n\n Envie "2" caso você não seja essa pessoa`
 const confirmation = "Confirmo."
 const notFound = "Não sou essa pessoa."
-const confirmationText = "Obrigado pela confirmação!"
+const confirmationText = "Obrigado pela confirmação."
 const badEnding = "Pedimos desculpas e agradecemos."
 const confirmation2 = "Isso me interessa."
 const confirmation3 = "Ainda tenho dúvidas."
-const question = `Tendo em vista que essa etapa pode se estender, saiba que você tem o direito de receber parte desse valor de forma antecipada por meio do crédito consignado em ações trabalhistas.\n Digite: "Isso me interessa" para confirmar \n Digite: "Ainda tenho dúvida", caso ainda esteja indeciso.`
+const question = `Considerando a fase em que seu processo se encontra, seu direito foi reconhecido e sua causa está ganha, faltando ainda a definição do valor e data de recebimento.`
+const question2 = `Tendo em vista que essa etapa pode se estender, saiba que você tem o direito de receber parte desse valor de forma antecipada por meio do crédito consignado em ações trabalhistas.\n\n Envie "1" para confirmar \n\n Envie "2" caso ainda tenha dúvidas.`
 const goodEnding = "Nosso consultor entrará em contato o mais brevemente possível. Obrigado."
 
-module.exports = { introduction, confirmation, notFound, confirmationText, badEnding, confirmation2, confirmation3, question, goodEnding }
+module.exports = { introduction, confirmation, notFound, confirmationText, badEnding, confirmation2, confirmation3, question, question2, goodEnding }
